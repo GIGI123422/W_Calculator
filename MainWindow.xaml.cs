fe505b7178fd7648912c34ed554f880b28a2efad
GorapaDuck <43196905+GIGI123422@users.noreply.github.com>
2021-12-23 15:24:11 +0900

    사칙연산 구현

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -20,9 +20,79 @@ namespace W_Calculator
     /// </summary>
     public partial class MainWindow : Window
     {
+        private bool opFlag = false; // true: 연산차 클릭된 직후, false: 아님
+        private double saved = 0; // 연산자 버튼이 클릭될 때 txtResult에 있던 값
+        private string op;
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        // 숫자 버튼 처리
+        private void btn_Click(object sender, RoutedEventArgs e)
+        {
+            Button btn = sender as Button;
+
+            if (txtResult.Text == "0" || opFlag == true)
+            {
+                txtResult.Text = btn.Content.ToString(); 
+                opFlag = false;
+            }
+            else
+                txtResult.Text += btn.Content.ToString();
+        }
+
+        // 소수점 처리
+        private void btnDot_Click(object sender, RoutedEventArgs e)
+        {
+            if (txtResult.Text.Contains("."))
+                return;
+            else
+                txtResult.Text += ".";
+        }
+
+        // PlusMinus 처리
+        private void btnPlusMinus_Click(object sender, RoutedEventArgs e)
+        {
+            double v = double.Parse(txtResult.Text);
+            v = -v;
+            txtResult.Text = v.ToString();
+        }
+
+        // 사칙연산자가 클릭되었을 때 처리
+        private void Op_Click(object sender, RoutedEventArgs e)
+        {
+            opFlag = true; // 숫자를 세로 써주기 위함
+            saved = double.Parse(txtResult.Text);
+
+            Button btn = sender as Button;
+            op = btn.Content.ToString();
+
+            txtExp.Text += txtResult.Text + " " + op;
+        }
+
+        // = 버튼 처리
+        private void btnEqual_Click(object sender, RoutedEventArgs e)
+        {
+            double v = double.Parse(txtResult.Text);
+
+            switch (op)
+            {
+                case "+":
+                    txtResult.Text = (saved + v).ToString();
+                    break;
+                case "-":
+                    txtResult.Text = (saved + v).ToString();
+                    break;
+                case "÷":
+                    txtResult.Text = (saved + v).ToString();
+                    break;
+                case "x":
+                    txtResult.Text = (saved + v).ToString();
+                    break;
+            }
+            txtExp.Text = saved + " " + op + v.ToString() + " =";
         }
     }
 }
